d8ca2399231e9e6775c594c7b5c434c9999fb84e
Katsumata <dai1nokira@gmail.com>
2020-09-19 01:08:22 +0900

    銃口の向きやら修正、エフェクトインポートした

---
+++ Assets/_Scripts/Attack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField] Vector3 rayDirection;
+    [SerializeField] float rayDistance = 1;
+    [SerializeField] GameObject muzzleExlodeEffect;
+    [SerializeField] GameObject muzzleExplodeEffectInsPosi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rayの作成　　　　　　　↓Rayを飛ばす原点　　　↓Rayを飛ばす方向
+        Ray ray = new Ray(transform.position, rayDirection * rayDistance);
+
+        //Rayが当たったオブジェクトの情報を入れる箱
+        RaycastHit hit;
+
+
+        //Rayの可視化    ↓Rayの原点　　　　↓Rayの方向　　　　　　　　　↓Rayの色
+        Debug.DrawLine(ray.origin, ray.origin + ray.direction * rayDistance, Color.red);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LaunchFireEffect();
+            if (Physics.Raycast(ray, out hit, rayDistance))
+            {
+                //Rayが当たったオブジェクトのtagがPlayerだったら
+                if (hit.collider.tag == "Enemy")
+                {
+                    Debug.Log("RayがEnemyに当たった");
+                    hit.collider.gameObject.GetComponent<EnemyDeath>().OnDead();
+                }
+            }
+        }
+        
+    }
+    void LaunchFireEffect()
+    {
+        Instantiate(muzzleExlodeEffect, muzzleExplodeEffectInsPosi.transform);
+    }
+}
---
+++ Assets/_Scripts/EnemyDeath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    bool death = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (death)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnDead()
+    {
+        death = true;
+    }
+}
