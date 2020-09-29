10ecb118f5d0de63f179822531b96bd9937411d4
Katsumata <dai1nokira@gmail.com>
2020-09-29 22:10:44 +0900

    beamエフェクト追加、要修正

---
+++ Assets/_Scripts/BeamController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamController : MonoBehaviour
+{
+    ParticleSystem[] particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponents<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            foreach (var p in particles)
+            {
+                p.Play();
+            }
+        }
+        else if(Input.GetButtonUp("Fire1"))
+        {
+            foreach (var p in particles)
+            {
+                p.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/ChangeParticleSystemEmitPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeParticleSystemEmitPosition : MonoBehaviour
+{
+    [SerializeField] Vector3 setPosi;
+    [SerializeField] ParticleSystem particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangePosition()
+    {
+        ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+        emitParams.position = setPosi;
+        emitParams.applyShapeToPosition = true;
+        particle.Emit(emitParams, 50);
+    }
+}
