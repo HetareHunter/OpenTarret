6632302c4422584cac15d61d4c0e40ed4ee865bb
Katsumata <dai1nokira@gmail.com>
2020-10-15 09:20:38 +0900

    VFX編集、エフェクトの再生時間、消滅時間編集

---
+++ Assets/_Prefabs/BeamVFX/Beam1Script/BeamDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamDestroy : MonoBehaviour
+{
+    bool deathCall = false;
+    [SerializeField] float untilDeath = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!deathCall)
+        {
+            Destroy(gameObject, untilDeath);
+            deathCall = true;
+        }
+    }
+}
