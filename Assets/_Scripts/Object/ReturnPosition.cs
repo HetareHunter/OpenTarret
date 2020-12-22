15161e7d2c04c0963a69501186ee41387889cf61
desk_katsu <dai1nokira@gmail.com>
2020-12-22 12:23:19 +0900

    やっぱりIKをたれっとに入れたほうが動かすのが楽だと思う

---
+++ Assets/_Scripts/Object/ReturnPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnPosition : MonoBehaviour
+{
+    Vector3 startPosition;
+    Quaternion startRotation;
+    [SerializeField] OVRGrabber oVRGrabberRight;
+    [SerializeField] OVRGrabber oVRGrabberLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (oVRGrabberRight.grabbedObject != gameObject && oVRGrabberLeft.grabbedObject != gameObject)
+        {
+            transform.position = startPosition;
+            transform.rotation = startRotation;
+        }
+    }
+
+
+}
