0e2e52ae083dff27697ff53645c6242bf2d9616a
desk_katsu <dai1nokira@gmail.com>
2021-01-12 12:20:01 +0900

    すばらしい・・・横回転を完璧に制御できるようになりましたよッッ！！

--- Assets/_Scripts/Player/HandleGrabbable.cs
+++ Assets/_Scripts/Player/HandleGrabbable.cs
@@ -16,10 +16,16 @@ namespace Players
         [SerializeField] Transform m_handle;
         [SerializeField, Range(0, 50.0f)] float handleRotateLimit;
         Vector3 m_preHandlePosi;
+        float m_preHandleToPlayerDis;
         float rotateAngle;
         /// <summary> ハンドルの感度 </summary>
         [SerializeField] float handleSensitivity = 2.0f;
 
+        [SerializeField] GameObject player;
+        /// <summary>
+        /// これ以上離れると自動的に手を放す
+        /// </summary>
+        [SerializeField] float playersArmLength = 0.6f; 
 
         public void GrabBegin(OVRInput.Controller controller)
         {
@@ -43,20 +49,27 @@ namespace Players
 
             if (isGrabbed && OVRInput.Get(OVRInput.Button.PrimaryHandTrigger, currentController))
             {
-                RotateHandle();
+                if (IsHoldOn())
+                {
+                    RotateHandle();
+                }
+                
             }
 
             if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger, currentController))
             {
                 returnPosition.Released();
                 ResetRotateHandle();
+                m_preHandleToPlayerDis = 0;
             }
             Debug.Log("currentcontroller:" + currentController);
         }
 
         void RotateHandle()
         {
-            float handleMoveDistance = (m_preHandlePosi.z - transform.localPosition.z) / Time.deltaTime * handleSensitivity;
+            float handleMoveDistance = MeasurementGrabToPlayer() / Time.deltaTime * handleSensitivity;
+            Debug.Log("handleMoveDistance : " + handleMoveDistance);
+            //float handleMoveDistance = (m_preHandlePosi.z - transform.localPosition.z) / Time.deltaTime * handleSensitivity;
             rotateAngle += handleMoveDistance;
             rotateAngle = Mathf.Clamp(rotateAngle, -handleRotateLimit, handleRotateLimit);
             m_handle.localRotation = Quaternion.AngleAxis(rotateAngle, Vector3.right);
@@ -66,6 +79,28 @@ namespace Players
             //}
             //m_handle.transform.Rotate(Vector3.right, rotateAngle, Space.Self);
             m_preHandlePosi = m_rotateTarget.localPosition;
+        }
+
+        /// <summary>
+        /// コライダーのプレイヤーとの距離に応じてハンドルが回るようにする
+        /// </summary>
+        float MeasurementGrabToPlayer()
+        {
+            float handleToPlayerDis = Vector3.Distance(transform.position, player.transform.position);
+            float preHandleToPlayerDis = m_preHandleToPlayerDis;
+            m_preHandleToPlayerDis = handleToPlayerDis;
+            
+            return preHandleToPlayerDis - handleToPlayerDis;
+        }
+
+        /// <summary>
+        /// プレイヤーはハンドルをつかめるかどうかの判定
+        /// 毎フレーム判定し、コライダーがタレットのハンドルから一定の距離以上離れたら手を放す
+        /// </summary>
+        /// <returns></returns>
+        bool IsHoldOn()
+        {
+            return Vector3.Distance(transform.position, player.transform.position) < playersArmLength;
         }
 
         void ResetRotateHandle()
