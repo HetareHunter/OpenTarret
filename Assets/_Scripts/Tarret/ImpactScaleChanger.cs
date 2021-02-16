a5b25ee3ed674af706f076ecf078cde64755bff6
desk_katsu <dai1nokira@gmail.com>
2021-02-17 00:01:25 +0900

    ショックウェーブの形をコーンにした

---
+++ Assets/_Scripts/Tarret/ImpactScaleChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ImpactScaleChanger : MonoBehaviour
+{
+    [SerializeField] Vector3 m_endSize;
+    [SerializeField] float m_time;
+
+    private void Start()
+    {
+        ChangeScale();
+    }
+    public void ChangeScale()
+    {
+        transform.DOScale(m_endSize, m_time)
+            .SetEase(Ease.Linear);
+    }
+}
