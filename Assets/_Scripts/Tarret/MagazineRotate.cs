00b1ba6864fab0461c0d1512e4930dced857c91f
desk_katsu <dai1nokira@gmail.com>
2021-02-16 20:13:43 +0900

    マガジンが安定して回転するようになった

--- Assets/_Scripts/Tarret/MagazineRotate.cs
+++ Assets/_Scripts/Tarret/MagazineRotate.cs
@@ -8,18 +8,26 @@ public class MagazineRotate : MonoBehaviour
     Quaternion nowAngle;
     [SerializeField] GameObject baseTarret;
     TarretAttack tarretAttack;
+    Animator magazineAnim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        nowAngle = transform.rotation;
+        //nowAngle = transform.rotation;
         tarretAttack = baseTarret.GetComponent<TarretAttack>();
+        magazineAnim = GetComponent<Animator>();
     }
 
 
     public void RotateMagazine()
     {
-        transform.DORotate(Vector3.forward * 60f, 1f, mode: RotateMode.LocalAxisAdd)
-            .OnComplete(() => tarretAttack.EndAttack());
+        //transform.DORotate(Vector3.forward * 60f, 1f, mode: RotateMode.LocalAxisAdd)
+        //    .OnComplete(() => tarretAttack.EndAttack());
+        magazineAnim.SetTrigger("RotateMagezine");
+    }
 
+    void AnimationComplete()
+    {
+        tarretAttack.EndAttack();
     }
 }
