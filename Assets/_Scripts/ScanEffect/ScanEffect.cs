b1ecbd65f23702406d59f0b14961b393b28d6a3e
desk_katsu <dai1nokira@gmail.com>
2021-03-12 16:09:19 +0900

    アンプリファイ入れた、scanlineシェーダ作った

---
+++ Assets/_Scripts/ScanEffect/ScanEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スキャンしていくオブジェクトに付ける。板状のもの
+/// </summary>
+public class ScanEffect : MonoBehaviour
+{
+    BoxCollider boxCollider;
+
+    [SerializeField] GameObject scanParticle;
+
+    [SerializeField] float effectLifeTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (var point in collision.contacts)
+        {
+            GameObject effect = Instantiate(scanParticle, point.point, Quaternion.identity);
+            Destroy(effect, effectLifeTime);
+        }
+    }
+}
