12c181e96aae143eb95997548becdb421bf45d33
Ykatsu999 <mhstkatsu@gmail.com>
2021-04-10 18:20:10 +0900

    t

--- Assets/_Scripts/GameManagement/GameStart.cs
+++ Assets/_Scripts/GameManagement/GameStart.cs
@@ -6,11 +6,53 @@ public class GameStart : MonoBehaviour
 {
     [SerializeField] float deathTime = 4.0f;
     [SerializeField] GameObject gameStartButton;
+
+    bool onLeftHand = false;
+    bool onRightHand = false;
+    public bool onHand = false;
+
+    private void Update()
+    {
+        OnHandJudge();
+    }
+
     public void StartGame()
     {
         GameStateManager.Instance.ChangeGameState(GameState.Start);
         this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 10, 1000));
         GetComponent<BoxCollider>().enabled = false;
         Destroy(gameStartButton, deathTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("RHand"))
+        {
+            onRightHand = true;
+        }
+        else if (other.gameObject.CompareTag("LHand"))
+        {
+            onLeftHand = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("RHand"))
+        {
+            onRightHand = false;
+        }
+        else if (other.gameObject.CompareTag("LHand"))
+        {
+            onLeftHand = false;
+        }
+    }
+
+    void OnHandJudge()
+    {
+        if (onLeftHand || onRightHand)
+        {
+            onHand = true;
+        }
     }
 }
