8e8c0abc30c8cfe86e163561c4cc75cec6f16aba
desk_katsu <dai1nokira@gmail.com>
2021-10-26 00:27:50 +0900

    MenuUIのコントローラのエミッション問題解決

--- Assets/_ScriptsMain/Player/UserGuid.cs
+++ Assets/_ScriptsMain/Player/UserGuid.cs
@@ -6,14 +6,6 @@ using UnityEngine.EventSystems;
 
 public class UserGuid : MonoBehaviour
 {
-    EventSystem _eventSystem;
-    EventTrigger _eventTrigger;
-    OVRInputModule _oVRInputModule;
-    OVRPointerEventData _oVRPointerEventData = new OVRPointerEventData(EventSystem.current);
-    [SerializeField] GameObject _oVRRaycasterObj;
-    OVRRaycaster _oVRRaycaster;
-    [SerializeField] GameObject _eventSystemObj;
-
     [SerializeField] GameObject _handMesh_L;
     [SerializeField] GameObject _handMesh_R;
     [SerializeField] GameObject _handController_L;
@@ -26,15 +18,16 @@ public class UserGuid : MonoBehaviour
     MaterialPropertyBlock _LControllerMpb;
     MaterialPropertyBlock _RControllerMpb;
 
+    [SerializeField] bool _onStartControllerMesh = false;
+
+    string _onEmissionID = "Boolean_373b9f6827e4408dba5a032c280ab463";
+
     void Awake()
     {
         _handMeshRenderer_L = _handMesh_L.GetComponent<SkinnedMeshRenderer>();
         _handMeshRenderer_R = _handMesh_R.GetComponent<SkinnedMeshRenderer>();
         _controllerMeshRenderer_L = _handController_L.GetComponent<Renderer>();
         _controllerMeshRenderer_R = _handController_R.GetComponent<Renderer>();
-
-        _oVRRaycaster = _oVRRaycasterObj.GetComponent<OVRRaycaster>();
-        _oVRInputModule = _eventSystemObj.GetComponent<OVRInputModule>();
     }
 
     // Start is called before the first frame update
@@ -42,11 +35,21 @@ public class UserGuid : MonoBehaviour
     {
         _LControllerMpb = new MaterialPropertyBlock();
         _RControllerMpb = new MaterialPropertyBlock();
-        _controllerMeshRenderer_L.enabled = false;
-        _controllerMeshRenderer_R.enabled = false;
 
-        _eventSystem = EventSystem.current;
-        _eventSystem.enabled = true;
+        if (_onStartControllerMesh)
+        {
+            _controllerMeshRenderer_L.enabled = true;
+            _controllerMeshRenderer_R.enabled = true;
+            _handMeshRenderer_L.enabled = false;
+            _handMeshRenderer_R.enabled = false;
+        }
+        else
+        {
+            _controllerMeshRenderer_L.enabled = false;
+            _controllerMeshRenderer_R.enabled = false;
+            _handMeshRenderer_L.enabled = true;
+            _handMeshRenderer_R.enabled = true;
+        }
     }
 
     public void SwicthHandMesh(bool useControllerMesh, Hand hand)
@@ -86,14 +89,14 @@ public class UserGuid : MonoBehaviour
     {
         if (button.GetComponent<Button>().interactable)
         {
-            _RControllerMpb.SetFloat(Shader.PropertyToID("Boolean_373b9f6827e4408dba5a032c280ab463"), 1.0f);
+            _RControllerMpb.SetFloat(Shader.PropertyToID(_onEmissionID), 1.0f);
             _controllerMeshRenderer_R.SetPropertyBlock(_RControllerMpb);
         }
     }
 
     public void NoUseEmissionAButton()
     {
-        _RControllerMpb.SetFloat(Shader.PropertyToID("Boolean_373b9f6827e4408dba5a032c280ab463"), 0.0f);
+        _RControllerMpb.SetFloat(Shader.PropertyToID(_onEmissionID), 0.0f);
         _controllerMeshRenderer_R.SetPropertyBlock(_RControllerMpb);
     }
 }
